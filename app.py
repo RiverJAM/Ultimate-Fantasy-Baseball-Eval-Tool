d436ec7414d85c46737d805105d0b0fc2813bd4e
Joseph Monahan <josephmonahan@Josephs-MacBook-Pro.local>
2021-02-13 17:46:15 -0600

    port change

--- app.py
+++ app.py
@@ -79,4 +79,5 @@ def hello_pitchers():
     return (jsonify(pitchers))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5050))
+    app.run(debug=True, host="0.0.0.0", port=port)
